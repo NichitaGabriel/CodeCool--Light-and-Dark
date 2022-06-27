40957c0c625b8e04f008c100e10e8c78d57fa9fb
Nichita Gabriel-Ionut <gabinichita1999@gmail.com>
2022-06-27 14:01:22 +0300

    added my solution

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function switchTheme() {
   // TODO: implement toggling the light/dark theme
+  let bodyElement = document.body;
+  let tableElement = document.getElementById("table-dark");
+
+  bodyElement.classList.toggle("dark");
+
+  if (bodyElement.classList.contains("dark")) {
+    tableElement.classList.add("table-dark");
+  } else tableElement.classList.remove("table-dark");
 }
